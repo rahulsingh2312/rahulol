debc3bdcc3bd29d14bfc2dcb13e6f870597848ee
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2023-09-30 04:12:32 +0530

    [foxes] about me section

--- rahulol/src/components/aboutme.jsx
+++ rahulol/src/components/aboutme.jsx
@@ -4,7 +4,7 @@ import Pfp from '../images/rahul.png';
 
 function aboutme(){
 return (
-    <div>
+    <div className='sm:mt-20 md:mt-0'>
 
     <div className='text-4xl ml-40  bg-black md:ml-40  md:p-40 md:text-6xl text-pink-300 '>
     <img src={Pfp} alt="pfp" className='md:ml-80' />
@@ -14,8 +14,8 @@ return (
        </div>
      
     </div>
-    <div className='text-white  z-1 text-sm helvetica bg-black md:opacity-0  ml-20 z-50 text-4xl sm:absolute'> 
-       Hiiii I'm A Tech Enthusiast and a learner 🧑‍💻, <br /> i usually talk in methaphors and javascript,<br />who is really optimistic and overconfident (ya i use that as a strength idk) , I'm Nocturnal🦉 and a bit delusional and sometimes I'm a Social Animal And a developer .
+    <div className='text-white  z-1 text-sm helvetica bg-black md:hidden ml-10 z-50 text-4xl absolute '> 
+       Hiiii I'm A Tech Enthusiast and a learner 🧑‍💻, i usually talk in methaphors and javascript,who is really optimistic and overconfident (ya i use that as a strength idk) , I'm Nocturnal🦉 and a bit delusional and sometimes I'm a Social Animal And a developer .
        </div>
     </div>
 );
