a8b15edac187808cf3084fe2a7ac5b2e6877c208
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2023-12-03 11:54:14 +0530

    audio done nrtlify

--- rahulol/src/components/aboutme.jsx
+++ rahulol/src/components/aboutme.jsx
@@ -33,10 +33,12 @@ useEffect(() => {
 
 return (
 
-    <div onClick={playRandomAudio} className='h-screen -mt-12   text-black-100' style={{backgroundColor:"#F4EAE1" , backgroundImage:`url(${Noise})`}}>
-   
-      <div style={{width: '20%', height: '20%', position: 'relative', background: 'linear-gradient(90deg, #FC5C7D 0%, #6A82FB 100%)', boxShadow: '200px 200px 200px ', borderRadius: 200, filter: 'blur(200px)'}} />
+    <div onClick={playRandomAudio} className='h-screen -mt-12   text-black-100' style={{backgroundColor:"#F4EAE1"}}>
+   <div className='absolute ' > <img src={Noise} style={{ width: '100vw', height: '100vh', animation: 'slideRight 8s linear infinite' }} alt='nos' /> </div>
+      <div style={{width: '20%', height: '20%', position: 'relative', background: 'linear-gradient(90deg, #FC5C7D 100%, #6A82FB 100%)', borderRadius: 400, filter: 'blur(40px)' , animation: 'slideRight 4s linear infinite'}} />
       <div className='flex justify-center' style={{ color: 'rgba(105, 111, 77, 0.80)', fontSize: 50, fontFamily: 'Inknut Antiqua', fontWeight: '100', lineHeight: 1.3, wordWrap: 'break-word'}}>RAHUL <br/>SINGH<br/>DEVELOPER<br/>DREAMER<br/>DELUSION<br/></div>
+      <div style={{width: '20%', height: '20%', position: 'absolute', right:'0',background: 'linear-gradient(90deg, #FC5C7D 100%, #6A82FB 100%)', borderRadius: 400, filter: 'blur(40px)' , animation: 'slideRight 4s linear infinite'}} />
+      <div style={{width: '20%', height: '20%', position: 'absolute', left:'50%', background: 'linear-gradient(90deg, #FC5C7D 100%, #6A82FB 100%)'  , borderRadius: 400, filter: 'blur(40px)' , animation: 'slideLeft 3s linear infinite'}} />
       <div className='md:flex md:justify-center md:-ml-36 mt-10 ml-10' style={{ color: 'rgba(0, 0, 0, 0.47)', fontSize: 16, fontFamily: 'Inknut Antiqua', fontWeight: '300', lineHeight: 2, wordWrap: 'break-word'}}>Tap Anywhere For Sound <br/>nd More Colors .</div>
     <div className=' image flex justify-end md:mt-36 mt-28 mr-5 ms:mr-10' > <img src={Soundon} alt="sndon" /></div>
     </div>
