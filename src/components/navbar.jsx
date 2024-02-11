b991f90f3bdee7833c7336eff0c2861194c8e34b
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2024-02-11 13:23:59 +0530

    resm

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -73,7 +73,7 @@ function Navbar({toggleNav}) {
               <a
               target='_blank' rel="noreferrer"
               // eslint-disable-next-line
-                href="https://drive.google.com/file/d/1KPtDwo_Ioo34cfPrNkOdsgzDUJZXF_mB/view?usp=sharing"
+                href="https://drive.google.com/file/d/1uMvYg2TJ1g2BhhrdyWyHnJ4fD6EmYpOi/view?usp=sharing"
                 className="block py-4 sm:border-zinc-100 sm:border-2  sm:mt-2 md:mt-0 pl-3 pr-4 md:mr-20 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-blue-700 md:p-0 dark:text-black dark:hover:bg-gray-700 dark:hover:text-grey md:dark:hover:bg-transparent"
               >
                 Resume
