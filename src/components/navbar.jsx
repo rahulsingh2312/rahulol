79a0b43e67b20f46c080b2f47ca9bf33c72ac582
Rahul <rahulsinghhh2312@gmail.com>
2024-04-12 21:57:50 +0530

    navbar blogs

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -106,6 +106,15 @@ function Navbar({ toggleNav }) {
                   Resume
                 </a>
               </li>
+              <li>
+                <a
+                  target='_blank' rel="noreferrer"
+                  href="/blog"
+                  className="block py-4 sm:border-zinc-100 sm:border-2 sm:mt-2 md:mt-0 pl-3 pr-4 md:mr-20 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-blue-700 md:p-0 dark:text-black dark:hover:bg-gray-700 dark:hover:text-grey md:dark:hover:bg-transparent"
+                >
+                  Blogs
+                </a>
+              </li>
             </ul>
           </div>
         </div>
