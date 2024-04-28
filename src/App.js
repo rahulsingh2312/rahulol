79bdae11dccba99ef44882c8952d93ad8d62758e
Rahul <rahulsinghhh2312@gmail.com>
2024-04-28 17:00:56 +0530

    practical exam copy

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Blog from './blog/page';
 import { Preloader } from './Preloader/Preloader';
 import { AnimatePresence } from 'framer-motion';
 import Practicals from './practicals/Practicals'
+
+import PracticalsD from './practicals/PracticalsD.js'
 export default function App() {
   const [loading, setLoading] = useState(true);
 
@@ -23,6 +25,7 @@ export default function App() {
       <Routes>
         <Route path="/blog" element={<Blog />} />
         <Route path="/p" element={<Practicals />} />
+        <Route path="/d" element={<PracticalsD />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
